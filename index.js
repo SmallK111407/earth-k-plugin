12270275c2f9126d3891bc45f75dbe3d7fdf634c
SunRyK <108290923+SmallK111407@users.noreply.github.com>
2022-09-24 21:19:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import fs from 'node:fs'
 
 logger.info('---------=.=---------')
 logger.info(`土块插件${currentVersion}载入成功~qwq`)
+logger.info(`土块插件群链接↓↓↓[禁止传播]`)
+logger.info(`https://jq.qq.com/?_wv=1027&k=KoHaEtds`)
 logger.info(`---------------------`);
 
 const files = fs
