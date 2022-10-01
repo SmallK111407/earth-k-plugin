c25027aeb8a4f8ffc2ef2f5c520034eb824d1eb5
SunRyK <108290923+SmallK111407@users.noreply.github.com>
2022-10-01 20:55:00 +0800

    #土块帮助 box底色透明度、毛玻璃修改

--- resources/help/theme/default/config.js
+++ resources/help/theme/default/config.js
@@ -13,12 +13,12 @@ export const style = {
   contBgColor: 'rgba(6, 21, 31, .5)',
 
   // 面板底图毛玻璃效果，数字越大越模糊，0-10 ，可为小数
-  contBgBlur: 3,
+  contBgBlur: 0,
 
   // 板块标题栏底色
-  headerBgColor: 'rgba(6, 21, 31, .4)',
+  headerBgColor: 'rgba(49, 56, 61, 0.1)',
   // 帮助奇数行底色
-  rowBgColor1: 'rgba(6, 21, 31, .2)',
+  rowBgColor1: 'rgba(30, 45, 54, 0.05)',
   // 帮助偶数行底色
-  rowBgColor2: 'rgba(6, 21, 31, .35)'
+  rowBgColor2: 'rgba(30, 45, 54, 0.05)'
 }
