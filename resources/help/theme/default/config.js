70849ec7125a3bf9e19eb4c310a133d7fbf91621
diqiushengwu <111220695+diqiushengwu@users.noreply.github.com>
2022-10-01 21:39:26 +0800

    土块帮助界面UI修改

--- resources/help/theme/default/config.js
+++ resources/help/theme/default/config.js
@@ -10,15 +10,15 @@ export const style = {
   /* 面板整体底色，会叠加在标题栏及帮助行之下，方便整体帮助有一个基础底色
   *  若无需此项可将rgba最后一位置为0即为完全透明
   *  注意若综合透明度较低，或颜色与主文字颜色过近或太透明可能导致阅读困难 */
-  contBgColor: 'rgba(6, 21, 31, .5)',
+  contBgColor: 'rgba(6, 21, 31, .4)',
 
   // 面板底图毛玻璃效果，数字越大越模糊，0-10 ，可为小数
   contBgBlur: 0,
 
   // 板块标题栏底色
-  headerBgColor: 'rgba(49, 56, 61, 0.1)',
+  headerBgColor: 'rgba(6, 21, 31, .4)',
   // 帮助奇数行底色
-  rowBgColor1: 'rgba(30, 45, 54, 0.05)',
+  rowBgColor1: 'rgba(6, 21, 31, .2)',
   // 帮助偶数行底色
-  rowBgColor2: 'rgba(30, 45, 54, 0.05)'
+  rowBgColor2: 'rgba(6, 21, 31, .4)'
 }
