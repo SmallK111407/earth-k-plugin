56c76e54fd9b61c4845605ecc8c120a0b3c1bf53
SunRyK <108290923+SmallK111407@users.noreply.github.com>
2022-10-04 23:39:16 +0800

    加载日志修改

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import fs from 'node:fs'
 
 logger.info('---------=.=---------')
 logger.info(`土块插件${currentVersion}载入成功~qwq`)
-logger.info(`土块插件群链接↓↓↓[禁止传播]`)
-logger.info(`https://jq.qq.com/?_wv=1027&k=KoHaEtds`)
+logger.info(`作者-SunRyK曉K & 地球生物`)
+logger.info(`注意！土块插件仅支持Yunzai V3`)
 logger.info(`---------------------`);
 
 const files = fs
