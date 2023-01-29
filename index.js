d49b401212aa312564fe82c0fcd4f2cba73c5fd0
SmallK111407 <3399280843@qq.com>
2023-01-30 06:53:03 +0800

    Fix

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 import { Data, Version } from './components/index.js'
 import fs from 'node:fs'
+import chalk from 'chalk'
 
-logger.info('---------=.=---------')
-logger.info(`土块插件${Version.version}载入成功~qwq`)
-logger.info(`作者-SunRyK曉K & 地球生物`)
-logger.info(`注意！土块插件仅支持Yunzai V3`)
-logger.info(`土块画图连不上官网，则安装此依赖，命令: pnpm add https-proxy-agent -w`)
-logger.info(`---------------------`);
+let ret = []
+
+logger.info(120, 255, 108)('---------=.=---------')
+logger.info(120, 255, 108)(`土块插件${Version.version}载入成功~qwq`)
+logger.info(120, 255, 108)(`作者-SunRyK曉K & 地球生物`)
+logger.info(120, 255, 108)(`土块画图连不上官网，则安装此依赖，命令: pnpm add https-proxy-agent -w`)
+logger.info(120, 255, 108)(`---------------------`);
 
 const files = fs
   .readdirSync('./plugins/earth-k-plugin/apps')
   .filter((file) => file.endsWith('.js'))
 
+  files.forEach((file) => {
+    ret.push(import(`./apps/${file}`))
+})
+
+ret = await Promise.allSettled(ret)
+
 let apps = {}
-for (let file of files) {
-  let name = file.replace('.js', '')
-  apps[name] = (await import(`./apps/${file}`))[name]
+for (let i in files) {
+  let name = files[i].replace('.js', '')
+  
+  if (ret[i].status != 'fulfilled') {
+    logger.error(`载入插件错误：${logger.red(name)}`)
+    logger.error(ret[i].reason)
+    continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
 export { apps }
