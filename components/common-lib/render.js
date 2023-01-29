b7ac4b80f048d69ff9c89b56b0d8f7ed60471c50
SmallK111407 <3399280843@qq.com>
2023-01-29 16:52:28 +0800

    Fix 改错误文案

--- components/common-lib/render.js
+++ components/common-lib/render.js
@@ -6,21 +6,21 @@ export default async function (path, params, cfg) {
   if (!e.runtime) {
     console.log('未找到e.runtime，请升级至最新版Yunzai')
   }
-  return e.runtime.render('expand-plugin', path, params, {
+  return e.runtime.render('earth-k-plugin', path, params, {
     retType: cfg.retMsgId ? 'msgId' : 'default',
     beforeRender ({ data }) {
       let resPath = data.pluResPath
-      const layoutPath = process.cwd() + '/plugins/expand-plugin/resources/common/layout/'
+      const layoutPath = process.cwd() + '/plugins/earth-k-plugin/resources/common/layout/'
       return {
         ...data,
         _res_path: resPath,
         _layout_path: layoutPath,
-        _tpl_path: process.cwd() + '/plugins/expand-plugin/resources/common/tpl/',
+        _tpl_path: process.cwd() + '/plugins/earth-k-plugin/resources/common/tpl/',
         defaultLayout: layoutPath + 'default.html',
         elemLayout: layoutPath + 'elem.html',
         sys: {
           scale: Cfg.scale(cfg.scale || 1),
-          copyright: `Created By Yunzai-Bot<span class="version">${Version.yunzai}</span> & Expand-Plugin<span class="version">${Version.version}</span>`
+          copyright: `Created By Yunzai-Bot<span class="version">${Version.yunzai}</span> & Earth-K-Plugin<span class="version">${Version.version}</span>`
         }
       }
     }
