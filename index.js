7aa15914da23e4124d938cbf06395f3ca6651cb5
SmallK111407 <3399280843@qq.com>
2023-01-30 06:55:48 +0800

    Fix

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import chalk from 'chalk'
 
 let ret = []
 
-logger.info(120, 255, 108)('---------=.=---------')
-logger.info(120, 255, 108)(`土块插件${Version.version}载入成功~qwq`)
-logger.info(120, 255, 108)(`作者-SunRyK曉K & 地球生物`)
-logger.info(120, 255, 108)(`土块画图连不上官网，则安装此依赖，命令: pnpm add https-proxy-agent -w`)
-logger.info(120, 255, 108)(`---------------------`);
+logger.info(chalk.rgb(120, 255, 108)(`---------=.=---------`))
+logger.info(chalk.rgb(120, 255, 108)(`土块插件${Version.version}载入成功~qwq`))
+logger.info(chalk.rgb(120, 255, 108)(`作者-SunRyK曉K & 地球生物`))
+logger.info(chalk.rgb(120, 255, 108)(`土块画图连不上官网，则安装此依赖，命令: pnpm add https-proxy-agent -w`))
+logger.info(chalk.rgb(120, 255, 108)(`---------------------`));
 
 const files = fs
   .readdirSync('./plugins/earth-k-plugin/apps')
