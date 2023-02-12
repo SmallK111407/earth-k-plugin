3daf92ef0efc578fa666bdcf6fc326b9dfad2c49
diqiushengwu <111220695+diqiushengwu@users.noreply.github.com>
2023-02-12 19:25:00 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -12,16 +12,38 @@ logger.info(chalk.rgb(120, 255, 108)(`---------------------`));
 
 try {
   await import('image-size')
+ 
   if (!await redis.get('earth-k:node_modules')) await redis.set('earth-k:node_modules', '1')
 } catch (error) {
   if (error.stack?.includes('Cannot find package')) {
     logger.warn('--------土块依赖缺失--------')
     logger.warn(`earth-k-plugin 缺少依赖将无法使用 ${logger.yellow('AI绘图')}`)
     logger.warn(`如需使用请运行：${logger.red('pnpm add image-size -w')}`)
+	 
     logger.warn('---------------------------')
     logger.debug(decodeURI(error.stack))
   } else {
     logger.error(`土块载入依赖错误：${logger.red('image-size')}`)
+	
+    logger.error(decodeURI(error.stack))
+  }
+  await redis.del('earth-k:node_modules')
+}
+try {
+
+  await import('fluent-ffmpeg')
+  if (!await redis.get('earth-k:node_modules')) await redis.set('earth-k:node_modules', '1')
+} catch (error) {
+  if (error.stack?.includes('Cannot find package')) {
+    logger.warn('--------土块依赖缺失--------')
+    logger.warn(`earth-k-plugin 缺少依赖将无法使用 ${logger.yellow('弹琴')}`)
+
+	 logger.warn(`如需使用请运行：${logger.red('pnpm add fluent-ffmpeg -w')}`)
+	 logger.warn(`或者使用：${logger.red('cnpm install fluent-ffmpeg -w')}`)
+    logger.warn('---------------------------')
+    logger.debug(decodeURI(error.stack))
+  } else {
+	 logger.error(`土块载入依赖错误：${logger.red('fluent-ffmpeg')}`)
     logger.error(decodeURI(error.stack))
   }
   await redis.del('earth-k:node_modules')
