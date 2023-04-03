620569f3da6034f56b4a7196c27c0b6d18ca5e78
diqiushengwu <111220695+diqiushengwu@users.noreply.github.com>
2023-04-03 14:01:50 +0800

    更新openai机器人功能，没那么容易触发了，然后改成了gpt3.5

--- apps/LittleTool.js
+++ apps/LittleTool.js
@@ -60,7 +60,7 @@ export class xgn extends plugin {
                 fnc: 'tqbz'
             }, {
                 /** 命令正则匹配 */
-                reg: "^机器人.*", //匹配消息正则,命令正则
+                reg: "^机器人(.*)$", //匹配消息正则,命令正则
                 /** 执行方法 */
                 fnc: 'jiqiren'
             }
@@ -70,57 +70,14 @@ export class xgn extends plugin {
     }
 
     async jiqiren(e) {
-        let msg = e.msg.replace(/机器人/, "").trim()
-        tempMsg = tempMsg + "\nHuman: " + msg
-        var data2 = {
-            prompt: tempMsg,
-            tokensLength: zs
+       
+        if (e.isMaster | e.isGroup) {
+            let msg = e.msg.replace(/机器人/g, "").trim()
+            let response4 = await fetch('https://api.caonm.net/api/ai/o.php?msg=' + msg);
+            let res = await response4.json()
+            res = res.Output
+            e.reply(res, true)
         }
-
-        let url = "https://api.forchange.cn/"
-
-
-        let res3 = await fetch(url, {
-            method: "post",
-
-            body: JSON.stringify(data2),
-            headers: {
-                'Content-Type': "application/json",
-                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
-            }
-
-        })
-        let res2 = await res3.json();
-        jieguo = res2.choices[0].text
-
-
-        if (jieguo == null) {
-            e.reply('重置聊天对话啦')
-
-
-
-
-            tempMsg = ""
-            return
-
-
-
-
-
-        }
-        jieguo = jieguo.replace(/\n/, "").trim()
-        jieguo = jieguo.replace(/答：/, "").trim()
-        jieguo = jieguo.replace(/Bot:/, "").trim()
-        jieguo = jieguo.replace(/robot:/, "").trim()
-        jieguo = jieguo.replace(/Robot:/, "").trim()
-        jieguo = jieguo.replace(/Computer:/, "").trim()
-        jieguo = jieguo.replace(/computer:/, "").trim()
-        jieguo = jieguo.replace(/AI:/, "").trim()
-
-        e.reply(jieguo, true)
-        tempMsg = ""
-        zs = tempMsg.length
-
 
 
     }
