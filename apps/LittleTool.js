e91567a0e3ce3b7b0438449c6156b280ffe61b3e
diqiushengwu <111220695+diqiushengwu@users.noreply.github.com>
2023-10-02 09:25:47 +0800

    修复今日运势

--- apps/LittleTool.js
+++ apps/LittleTool.js
@@ -191,17 +191,31 @@ export class xgn extends plugin {
     console.log(res)
   }
   async jrys (e) {
-     let url = 'https://api.fanlisky.cn/api/qr-fortune/get/' + String(e.user_id)
-        let res = await fetch(url)
-        res = await res.json()
-        res =res.data
-
-        let fortuneSummary = res.fortuneSummary
-        let luckyStar = res.luckyStar
-        let signText = res.signText
-        let unSignText = res.unSignText
-       let msg = `运势：${fortuneSummary}\n星级：${luckyStar}\n点评：${signText}\n解读：${unSignText}`
-        e.reply([segment.at(e.user_id), '\n', msg])
+     try{
+            let url = 'http://itukuai.top:1450/jrys.php?qq=' + String(e.user_id)
+            let res = await fetch(url)
+            res = await res.json()
+            
+    
+            let fortuneSummary = res.fortuneSummary
+            let luckyStar = res.luckyStar
+            let signText = res.signText
+            let unSignText = res.unSignText
+            let msg = `运势：${fortuneSummary},\n星级：${luckyStar}\n点评：${signText}\n解读：${unSignText}`
+            e.reply([segment.at(e.user_id), '\n', msg])
+        }catch{
+            let url = 'https://api.fanlisky.cn/api/qr-fortune/get/' + String(e.user_id)
+            let res = await fetch(url)
+            res = await res.json()
+            res =res.data
+    
+            let fortuneSummary = res.fortuneSummary
+            let luckyStar = res.luckyStar
+            let signText = res.signText
+            let unSignText = res.unSignText
+            let msg = `运势：${fortuneSummary},\n星级：${luckyStar}\n点评：${signText}\n解读：${unSignText}`
+            e.reply([segment.at(e.user_id), '\n', msg])
+        }
   }
 
   async bqhc(e) {
