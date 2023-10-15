b4d328bbac80aa4d601b1bf0079454ca7b06a853
diqiushengwu <111220695+diqiushengwu@users.noreply.github.com>
2023-10-15 14:39:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,6 @@ let ret = []
 logger.info(chalk.rgb(120, 255, 108)(`---------=.=---------`))
 logger.info(chalk.rgb(120, 255, 108)(`土块插件${Version.version}载入成功~qwq`))
 logger.info(chalk.rgb(120, 255, 108)(`作者-SunRyK曉K & 地球生物`))
-logger.info(chalk.rgb(120, 255, 108)(`土块画图连不上官网，则安装此依赖，命令: pnpm add https-proxy-agent -w`))
 logger.info(chalk.rgb(120, 255, 108)(`---------------------`));
 
 try {
