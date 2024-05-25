5abed782055bc60feda3bc0102979b7f58b640cb
diqiushengwu <111220695+diqiushengwu@users.noreply.github.com>
2024-05-25 17:41:12 +0800

    大更新！

--- apps/shareMusic.js
+++ apps/shareMusic.js
@@ -116,10 +116,10 @@ export class ShareMusic extends plugin {
 
 
 		const urlList = {
-			qq: 'http://www.itukuai.top:1450/djs/API/QQ_Music/api.php?msg=paramsSearch&limit=30',
+			qq: 'https://itukuai.top/djs/API/QQ_Music/api.php?msg=paramsSearch&limit=30',
 			kugou:
 				'http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword=paramsSearch&page=1&pagesize=10&showtype=1',
-			wangyiyun: 'http://itukuai.top:3000/search?keywords=paramsSearch',//备用API：http://www.clearfor.xyz:3000/cloudsearch?keywords=paramsSearch
+			wangyiyun: 'https://music.itukuai.top/search?keywords=paramsSearch',//备用API：http://www.clearfor.xyz:3000/cloudsearch?keywords=paramsSearch
 			//https://autumnfish.cn/search?keywords=paramsSearch
 			//https://music.cyrilstudio.top/search?keywords=paramsSearch
 
