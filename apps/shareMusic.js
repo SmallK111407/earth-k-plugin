18d5edf1b8d663a007a05f938b776072369df16d
OverTimeBunny <v17360963@gmail.com>
2024-10-22 17:52:49 +0000

    !23 【轻量级 PR】：加个提示，避免等待过久 Merge pull request !23 from OverTimeBunny/N/A

--- apps/shareMusic.js
+++ apps/shareMusic.js
@@ -52,49 +52,51 @@ export class ShareMusic extends plugin {
 		})
 	}
 async wybofang(e) {
-			let ckxx = YAML.parse(
-			fs.readFileSync('./plugins/earth-k-plugin/config/config.yaml', 'utf8')
-		);
-		wyck = ckxx.wyck
+		e.reply('正在下载音乐文件，请稍等~'); // 添加提示
+		let ckxx = YAML.parse(
+		fs.readFileSync('./plugins/earth-k-plugin/config/config.yaml', 'utf8')
+	);
+	wyck = ckxx.wyck
 
-		let ge = e.msg.replace(/网易播放/g, "").trim()
-		if (ge == '') return e.reply('请输入要播放的歌曲')
-		let url = `https://music.itukuai.top/search?keywords=${ge}`
-		let response2 = await fetch(url);
-		const data2 = await response2.json();
-		
-		let ids = String(data2.result.songs[0].id)
-		url = 'http://music.163.com/song/media/outer/url?id=' + ids
+	let ge = e.msg.replace(/网易播放/g, "").trim()
+	if (ge == '') return e.reply('请输入要播放的歌曲')
+	let url = `https://music.itukuai.top/search?keywords=${ge}`
+	let response2 = await fetch(url);
+	const data2 = await response2.json();
+	
+	let ids = String(data2.result.songs[0].id)
+	url = 'http://music.163.com/song/media/outer/url?id=' + ids
 
-		let options = {
-			method: 'POST',//post请求 
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 12; MI Build/SKQ1.211230.001)',
-				'Cookie': 'versioncode=8008070; os=android; channel=xiaomi; ;appver=8.8.70; ' + "MUSIC_U=" + wyck
-			},
-			body: `ids=${JSON.stringify([ids])}&level=standard&encodeType=mp3`
-		};
-		let response = await fetch('https://music.163.com/api/song/enhance/player/url/v1', options); //调用接口获取数据
-		let res = await response.json(); //结果json字符串转对象
-		if (res.code == 200) {
-			url = res.data[0]?.url;
-			url = url ? url : '';
-		}
-		try {
-			let msg = await uploadRecord(url, 0, false)
-			e.reply(msg)
-		} catch {
-			let msg2 = await segment.record(url)
-
-			await e.reply(msg2)
-		}
-
-
-		await SendMusicShare(e, { source: 'netease', name: data2.result.songs[0].name, artist: data2.result.songs[0].artists[0].name, pic: data2.result.songs[0].artists[0].img1v1Url, link: 'https://music.163.com/#/song?id=' + ids, url: url })
-
+	let options = {
+		method: 'POST',//post请求 
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 12; MI Build/SKQ1.211230.001)',
+			'Cookie': 'versioncode=8008070; os=android; channel=xiaomi; ;appver=8.8.70; ' + "MUSIC_U=" + wyck
+		},
+		body: `ids=${JSON.stringify([ids])}&level=standard&encodeType=mp3`
+	};
+	let response = await fetch('https://music.163.com/api/song/enhance/player/url/v1', options); //调用接口获取数据
+	let res = await response.json(); //结果json字符串转对象
+	if (res.code == 200) {
+		url = res.data[0]?.url;
+		url = url ? url : '';
 	}
-	async bofang(e) {
+	try {
+		let msg = await uploadRecord(url, 0, false)
+		e.reply(msg)
+	} catch {
+		let msg2 = await segment.record(url)
+
+		await e.reply(msg2)
+	}
+
+
+	await SendMusicShare(e, { source: 'netease', name: data2.result.songs[0].name, artist: data2.result.songs[0].artists[0].name, pic: data2.result.songs[0].artists[0].img1v1Url, link: 'https://music.163.com/#/song?id=' + ids, url: url })
+
+}
+async bofang(e) {
+		e.reply('正在下载音乐文件，请稍等~'); // 添加提示
 		let ge = e.msg.replace(/播放/g, "").trim()
 		if (ge == '') return e.reply('请输入要播放的歌曲')
 		let url = `http://datukuai.top:1450/djs/API/QQ_Music/api.php?msg=${ge}&n=1&q=7`
@@ -183,9 +185,6 @@ async wybofang(e) {
 		let isKugou = isKugouReg.test(msg);
 		let isWangYiyun = isWangYiyunReg.test(msg);
 
-
-
-
 		if (e.msg.includes("qq")) {
 			isQQ = 1
 		} else if (e.msg.includes("酷狗")) {
@@ -193,10 +192,6 @@ async wybofang(e) {
 		} else {
 			isWangYiyun = 1
 		}
-
-
-
-
 
 		if (!isQQ && !isKugou && !isWangYiyun) isWangYiyun = true;
 		if (msg.includes("点歌")) {
@@ -479,10 +474,6 @@ async wybofang(e) {
 
 					await e.reply(msg2)
 				}
-
-
-
-
 
 				await SendMusicShare(e,{source: 'netease',name:songList[id-1].name,artist:songList[id-1].artists[0].name,pic:songList[id-1].artists[0].img1v1Url,link:'https://music.163.com/#/song?id='+ids,url:url})
 
